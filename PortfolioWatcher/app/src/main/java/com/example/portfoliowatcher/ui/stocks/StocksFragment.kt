3bc98b15d62491270b1accaaa0abb0e2cbe75b99
Ugar, Emin <emin.ugar@orioninc.com>
2023-06-15 13:23:34 +0300

    fragments moved to packages
    fragments moved to packages

--- PortfolioWatcher/app/src/main/java/com/example/portfoliowatcher/ui/stocks/StocksFragment.kt
+++ PortfolioWatcher/app/src/main/java/com/example/portfoliowatcher/ui/stocks/StocksFragment.kt
@@ -1,10 +1,11 @@
-package com.example.portfoliowatcher
+package com.example.portfoliowatcher.ui.stocks
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.portfoliowatcher.R
 
 
 class StocksFragment : Fragment() {
