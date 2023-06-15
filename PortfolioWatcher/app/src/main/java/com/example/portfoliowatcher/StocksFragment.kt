8e88e51ed97a05bf8aafaa2fe2b4fc30b28793e6
Ugar, Emin <emin.ugar@orioninc.com>
2023-06-15 13:21:43 +0300

    home fragment name changed to stocks fragment
    home fragment name changed to stocks fragment

--- PortfolioWatcher/app/src/main/java/com/example/portfoliowatcher/StocksFragment.kt
+++ PortfolioWatcher/app/src/main/java/com/example/portfoliowatcher/StocksFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class HomeFragment : Fragment() {
+class StocksFragment : Fragment() {
 
 
     override fun onCreateView(
@@ -15,7 +15,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_stocks, container, false)
     }
 
 }
