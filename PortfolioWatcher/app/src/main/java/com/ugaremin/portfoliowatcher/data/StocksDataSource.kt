6f851bc197027d7cc22f9acc51e248e25645c834
Emin UGAR <33444078+ugaremin@users.noreply.github.com>
2024-02-29 13:16:46 +0300

    get stock datail

--- PortfolioWatcher/app/src/main/java/com/ugaremin/portfoliowatcher/data/StocksDataSource.kt
+++ PortfolioWatcher/app/src/main/java/com/ugaremin/portfoliowatcher/data/StocksDataSource.kt
@@ -1,12 +1,15 @@
 package com.ugaremin.portfoliowatcher.data
 
 import android.content.Context
+import android.util.Log
 import com.ugaremin.portfoliowatcher.data.Room.AppDatabase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
 
 class StocksDataSource {
+
+    private var urlPattern = """<a\s+href="([^"]+)".*?>""".toRegex()
 
     suspend fun getStocksData(): MutableList<StocksData> {
         val url = "https://borsa.doviz.com/hisseler"
@@ -23,9 +26,12 @@ class StocksDataSource {
             val columns = row.select("td")
             if (columns.size >= 5) {
                 val stockName = columns[0].text().trim()
+                val stockUrl = columns[0].html().toString()
+                val matchResult = urlPattern.find(stockUrl)
+                val getUrl = matchResult?.groupValues?.get(1).toString()
                 val lastValue = columns[2].text().trim()
                 val percentChange = columns[6].text().trim()
-                val rowData = StocksData(stockName, lastValue, percentChange)
+                val rowData = StocksData(stockName, lastValue, percentChange, getUrl)
                 data.add(rowData)
             }
         }
@@ -50,12 +56,40 @@ class StocksDataSource {
                 val lastValue = columns[2].text().trim()
                 val percentChange = columns[6].text().trim()
                 val name = stockName.take(5)
+                val stockUrl = columns[0].html().toString()
+                val matchResult = urlPattern.find(stockUrl)
+                val getUrl = matchResult?.groupValues?.get(1).toString()
                 val user = AppDatabase.getInstance(context).stocksDao().findStock(name)
                 if(user != null){
-                    val rowData = StocksData(stockName, lastValue, percentChange)
+                    val rowData = StocksData(stockName, lastValue, percentChange, getUrl)
                     data.add(rowData)
                 }
 
+            }
+        }
+        return data
+    }
+
+    suspend fun getStocksDetail(stockUrl: String): MutableList<StockDetailData> {
+        val url = stockUrl
+        val doc = withContext(Dispatchers.IO) {
+            Jsoup.connect(url).get()
+        }
+
+        val table = doc.select("table")
+        val tableRows = table.select("tr")
+
+        val data = mutableListOf<StockDetailData>()
+
+        for (row in tableRows) {
+            val columns = row.select("td")
+            if (columns.size >= 4) {
+                val weeklyChange = columns[1].text().trim()
+                val monthlyChange = columns[2].text().trim()
+                val yearlyChange = columns[3].text().trim()
+                Log.i("EMN", "weeklyChange: $weeklyChange monthlyChange: $monthlyChange yearlyChange: $yearlyChange")
+                val rowData = StockDetailData(weeklyChange, monthlyChange, yearlyChange)
+                data.add(rowData)
             }
         }
         return data
